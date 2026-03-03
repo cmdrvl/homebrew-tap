d7429e53fda33698d51dc4c03e4f75e0651c07ea
Zachary Ruiz <zac.ruiz@salt.io>
2026-03-03 09:31:49 -0500

    chore: update fingerprint Homebrew formula to v0.3.0

--- Formula/fingerprint.rb
+++ Formula/fingerprint.rb
@@ -1,32 +1,22 @@
-# typed: false
-# frozen_string_literal: true
-
 class Fingerprint < Formula
-  desc "Deterministic structural fingerprint for CSV datasets"
+  desc "Determine whether an artifact matches a known template"
   homepage "https://github.com/cmdrvl/fingerprint"
-  version "0.1.0"
-  license "MIT"
+  version "0.3.0"
+  license "Proprietary"
 
   on_macos do
-    on_arm do
-      url "https://github.com/cmdrvl/fingerprint/releases/download/v0.1.0/fingerprint-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "bd5fcf7b804a12b281f3416b17b444149ecbd7e4c11b96ae6531e22d17941044"
-    end
-    on_intel do
-      url "https://github.com/cmdrvl/fingerprint/releases/download/v0.1.0/fingerprint-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "afae43974bc006b822c5098584734fd00e16462e48fcec7bfabf23cc63778b04"
+    if Hardware::CPU.arm?
+      url "https://github.com/cmdrvl/fingerprint/releases/download/v0.3.0/fingerprint-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "a6fc05f7b68c59a994fc45b11383a568c72155f7616f9cd7ef16857cec4d25a6"
+    else
+      url "https://github.com/cmdrvl/fingerprint/releases/download/v0.3.0/fingerprint-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "4af1e6942241ba0fa2653e59ffeb9f8ce680e11a2ee797dfebc89ddbc7b78a93"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/cmdrvl/fingerprint/releases/download/v0.1.0/fingerprint-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "c9c443c6732281cfc724b0ec56abcbf2ae6396250d50119eb2ba77c3bf8284cb"
-    end
-    on_intel do
-      url "https://github.com/cmdrvl/fingerprint/releases/download/v0.1.0/fingerprint-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "dbb7c587dcdcfbd4559922613b3439c65c400ed3e41ec8b85be85b4cf6647627"
-    end
+    url "https://github.com/cmdrvl/fingerprint/releases/download/v0.3.0/fingerprint-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "17af37b9c5d021822238201f8bb8e4569177d16d0dc840f168c5c39f4a81ca14"
   end
 
   def install
