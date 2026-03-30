b04c42bb9c6c4761a83ad5e7be4e833757755630
Zac Ruiz <zac@salt.io>
2026-03-30 15:57:22 -0400

    chore: update veil Homebrew formula to v0.2.2

--- Formula/veil.rb
+++ Formula/veil.rb
@@ -1,28 +1,28 @@
 class Veil < Formula
   desc "Data exfiltration guard for AI coding agents"
   homepage "https://github.com/cmdrvl/veil"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/cmdrvl/veil/releases/download/v0.2.1/veil-v0.2.1-darwin-arm64.tar.gz"
-      sha256 "dbf7d83440b89aaac9d99414e57970d21759beadab7039008d67cb8fa462d617"
+      url "https://github.com/cmdrvl/veil/releases/download/v0.2.2/veil-v0.2.2-darwin-arm64.tar.gz"
+      sha256 "00e7d179b0a4b8e8f3527d9166484f40689b0ec52aa5dab40901ddca40e3e21c"
     end
     on_intel do
-      url "https://github.com/cmdrvl/veil/releases/download/v0.2.1/veil-v0.2.1-darwin-x86_64.tar.gz"
-      sha256 "481c13e9eedf15474712863afafce74bb0b1dc63e3985bbedd41406681ebb299"
+      url "https://github.com/cmdrvl/veil/releases/download/v0.2.2/veil-v0.2.2-darwin-x86_64.tar.gz"
+      sha256 "32a5d54ff2ae5bd30404922b55f482621ce1808fc9894f9e25ae6fe756bd96c4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cmdrvl/veil/releases/download/v0.2.1/veil-v0.2.1-linux-arm64.tar.gz"
-      sha256 "6e90bae8172beb1e13dca733ab5bfa8d892bd95af67ce8566c2b3a28ee31b751"
+      url "https://github.com/cmdrvl/veil/releases/download/v0.2.2/veil-v0.2.2-linux-arm64.tar.gz"
+      sha256 "c804523494c87a2dc2800582174c546d5612db84b38a99d5bd4324b3f9d0ffe9"
     end
     on_intel do
-      url "https://github.com/cmdrvl/veil/releases/download/v0.2.1/veil-v0.2.1-linux-x86_64.tar.gz"
-      sha256 "a649e6945bed33892aa8d84b34f0e09aba6936c36a8acf1c34e498d45703ee3f"
+      url "https://github.com/cmdrvl/veil/releases/download/v0.2.2/veil-v0.2.2-linux-x86_64.tar.gz"
+      sha256 "13b5e4176c1e5d1a4a16b4c943167f5d2a4ff4199d5c1d3ec6f8e6e784d98f77"
     end
   end
 
