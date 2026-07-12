467bc10b74d569aa2c63ac72caa6e056c4b30d1d
Zac <zac@cmdrvl.com>
2026-07-12 09:17:12 -0400

    bd-2br: update decoding formula to 0.4.0

--- Formula/decoding.rb
+++ Formula/decoding.rb
@@ -8,23 +8,23 @@ class Decoding < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/cmdrvl/decoding/releases/download/v0.3.0/decoding-v0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "8bd49d412d320aa340b9a9306d21c2120fe49a34de7cda6e864ca4a5f6009282"
+      url "https://github.com/cmdrvl/decoding/releases/download/v0.4.0/decoding-v0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "85e200db01e066f009eaef79b7989eb995b81dff5ddb12686217408a37398000"
     end
     on_intel do
-      url "https://github.com/cmdrvl/decoding/releases/download/v0.3.0/decoding-v0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "93547020862008665a31962fa50c6f2931dfb7a4d748ec1df253b1158bbbba3b"
+      url "https://github.com/cmdrvl/decoding/releases/download/v0.4.0/decoding-v0.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "32f802cab207f527685e9458eb02478cbc5e946b8235b7deda28cc1bdbab98b7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/cmdrvl/decoding/releases/download/v0.3.0/decoding-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "2be6833f1a539a0d35e9b84d5fd11238034f3147850d907d20483fab0e9cb8b9"
+      url "https://github.com/cmdrvl/decoding/releases/download/v0.4.0/decoding-v0.4.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e971de9e9d301de7b165e426119e769fa055b14a449f49ec434deac0bc18dd78"
     end
     on_intel do
-      url "https://github.com/cmdrvl/decoding/releases/download/v0.3.0/decoding-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cdf57420bede8b270e3b6c417fdea8bacefc3fd80698e94b3cf75c7c87060d44"
+      url "https://github.com/cmdrvl/decoding/releases/download/v0.4.0/decoding-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "33066f6f409caf6340f5e2d7c6d2ba2a71d68cd547d925f903d4997649416a30"
     end
   end
 
